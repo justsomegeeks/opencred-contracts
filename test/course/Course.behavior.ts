72f9e8d477d3aac7735c903dbd37e5ad18be982f
chiranz <chiranpoudyal@gmail.com>
2021-12-26 11:36:31 +0530

    fix: remove unnecessary imports

--- test/course/Course.behavior.ts
+++
@@ -1,7 +0,0 @@
-import { expect } from "chai";
-
-export function shouldBehaveLikeCourse(): void {
-  it("should return course cid", async function () {
-    expect(await this.course.getCourseCID()).to.equal("ipfs://coursecid");
-  });
-}
